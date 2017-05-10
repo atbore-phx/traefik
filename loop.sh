7c6f62deacd789b98b8c64b4621956ef7968d96a
Attilio Borello <attilio@containo.us>
2017-05-10 11:32:10 +0200

    job 1

--- loop.sh
+++ loop.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 
-NLOOP=20
-
-for i in {1..$NLOOP}
+for i in {1..20}
 do
-
+echo "job$1"
 echo $RANDOM >> random_test
 git add .
 git commit -m "job $i"
 git push origin loop-flaking
 sleep 2
-
 done 
